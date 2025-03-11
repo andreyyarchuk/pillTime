3c894345ab822edadac24428d4a6c45798446f89
andreyyarchuk <dron4432@gmail.com>
2025-03-11 16:16:17 +0200

    add Http-request

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return {"massage": "Hello, world!"}
+@app.route("/api", methods=["GET"])
+def getData():
+    return jsonify({"message": "GET-request"})
+
+@app.route("/api", methods=["POST"])
+def createData():
+    data = request.get_json() # Получаем данные в формате JSON
+    if not data:
+        return jsonify({"error":"Invalid JSON"}), 400
+    name = data.get('name', 'Unknown')
+    return jsonify({"message":f"Hello, {name}! This is POST-request test"}), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
